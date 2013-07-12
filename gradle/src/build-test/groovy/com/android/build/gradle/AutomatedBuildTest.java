a4b1603164dfd8ef64ffe5d9ea8086916c1c40c0
Xavier Ducrohet <xav@android.com>
2013-07-12 10:46:11 -0700

    Fix bad property access in the DependencyChecker.
    Add a new test project to check this code.
    
    Change-Id: Icd19941ae55ad4c51e0962bde4994e6e6ac954fd
    (cherry picked from commit 4983ea711f4218687965d2b6a5fa97db7afcc33b)

--- gradle/src/build-test/groovy/com/android/build/gradle/AutomatedBuildTest.java
+++ gradle/src/build-test/groovy/com/android/build/gradle/AutomatedBuildTest.java
@@ -34,11 +34,11 @@ public class AutomatedBuildTest extends BuildTest {
     private static enum TestType { BUILD, REPORT }
 
     private static final String[] sBuiltProjects = new String[] {
-            "aidl", "api", "applibtest", "assets", "attrOrder", "basic", "dependencies", "flavored",
-            "flavorlib", "flavors", "libTestDep", "libsTest", "localJars", "migrated",
-            "multiproject", "multires", "overlay1", "overlay2", "pkgOverride", "proguard",
-            "proguardLib", "renderscript", "renderscriptInLib", "renderscriptMultiSrc",
-            "sameNamedLibs", "tictactoe" /*, "autorepo"*/
+            "aidl", "api", "applibtest", "assets", "attrOrder", "basic", "dependencies",
+            "dependencyChecker", "flavored", "flavorlib", "flavors", "libTestDep", "libsTest",
+            "localJars", "migrated", "multiproject", "multires", "overlay1", "overlay2",
+            "pkgOverride", "proguard", "proguardLib", "renderscript", "renderscriptInLib",
+            "renderscriptMultiSrc", "sameNamedLibs", "tictactoe" /*, "autorepo"*/
     };
 
     private static final String[] sReportProjects = new String[] {
